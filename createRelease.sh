8135e74e37caabefb9f38611369e18fd5c6e4973
Jordan Walke <jordojw@gmail.com>
2018-01-07 00:38:10 -0800

    Make version for lastest bs-platform

--- createRelease.sh
+++ createRelease.sh
@@ -14,9 +14,12 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+
 UPSTREAM_VERSION="$1"
 UPSTREAM_CHECKSUM_FILE="${SCRIPTDIR}/packageInfo/upstreamChecksums/${UPSTREAM_VERSION}.txt"
 VERSION_MAPPING_FILE="${SCRIPTDIR}/packageInfo/esy-ocaml-versionMapping/${UPSTREAM_VERSION}.upstream.txt"
+
+echo "Creating esy release for upstream version: ($UPSTREAM_VERSION)"
 
 if [ -f "${UPSTREAM_CHECKSUM_FILE}" ]; then
   true
@@ -47,6 +50,10 @@ if [ "$SHA_SUM_CHECKED" != "${SHA_SUM}  ${RELEASE_FILE}" ]; then
   exit 1
 fi
 
+# Remove any left over package
+rm -rf ./package/
+
+
 # Extracts here, there will be a package/
 tar -xvf "$RELEASE_FILE"
 # These flags work on mac - not tested on linux.
@@ -58,15 +65,40 @@ sed -i '.bak' "s/_VERSION_/${ESY_OCAML_VERSION}/g" esy.json
 sed -i '.bak' 's/bs-platform/@esy-ocaml\/bs-platform/g' package/package.json
 sed -i '.bak' "s/\"version\": \"${UPSTREAM_VERSION}\"/\"version\": \"${ESY_OCAML_VERSION}\"/g" package/package.json
 
-if [ "${VERSION}" == "2.0.0" ]; then
+if [ "${UPSTREAM_VERSION}" == "2.0.0" ]; then
+  echo "Backing up jscomp/bin/bsb.ml"
   mv package/jscomp/bin/bsb.ml package/jscomp/bin/bsb.ml.orig
   patch ./package/jscomp/bin/bsb.ml.orig -i "./packageInfo/upstreamPatches/${UPSTREAM_VERSION}/jscomp/bin/bsb.ml.patch" -o ./package/jscomp/bin/bsb.ml
 fi
 
-if [ "${VERSION}" == "2.1.0" ]; then
-  rm -f ./package/lib/*.darwin
-  rm -f ./package/lib/*.linux64
-  rm -f ./package/lib/*.win
+if [ "${UPSTREAM_VERSION}" == "2.1.0" ]; then
+  echo "Removing executable files in release:"
+ 
+  rm package/lib/bsb.darwin
+  rm package/lib/bsb_helper.darwin
+  rm package/lib/bsc.darwin
+  rm package/lib/bsppx.darwin
+  rm package/lib/reactjs_jsx_ppx_2.darwin
+  rm package/lib/refmt.darwin
+  rm package/lib/refmt3.darwin
+
+  # Prebuilts for linux aren't distributed.
+  # rm package/lib/bsb.linux64
+  # rm package/lib/bsb_helper.linux64
+  # rm package/lib/bsc.linux64
+  # rm package/lib/bsppx.linux64
+  # rm package/lib/reactjs_jsx_ppx_2.linux64
+  # rm package/lib/refmt.linux64
+  # rm package/lib/refmt3.linux64
+
+  rm package/lib/bsb.win
+  rm package/lib/bsb_helper.win
+  rm package/lib/bsc.win
+  rm package/lib/bsppx.win
+  rm package/lib/reactjs_jsx_ppx_2.win
+  rm package/lib/refmt.win
+  rm package/lib/refmt3.win
+
   mv ./package/lib/bsb.ml ./package/lib/bsb.ml.orig
   patch ./package/lib/bsb.ml.orig -i "./packageInfo/upstreamPatches/${UPSTREAM_VERSION}/lib/bsb.ml.patch" -o ./package/lib/bsb.ml
 fi
