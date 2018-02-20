1ab8eb1110b408511c12e2adfda45156442f6159
Jordan Walke <jordojw@gmail.com>
2018-02-20 03:17:11 -0800

    Simplify releases

--- createRelease.sh
+++ createRelease.sh
@@ -9,46 +9,30 @@ done
 SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 if [ -z "$1" ]; then
-  echo "You must supply an upstream version like this:"
-  echo "./createRelease.sh 2.0.0"
+  echo "You must supply a version of bs-platform, as well as a corresponding version for @esy-ocaml/bs-platform:"
+  echo "./createRelease.sh 2.0.0 2.0.0"
+  echo "or"
+  echo "./createRelease.sh 2.3.5 2.3.5000"
   exit 1
 fi
 
+if [ -z "$2" ]; then
+  echo "You must supply a version of bs-platform, as well as a corresponding version for @esy-ocaml/bs-platform:"
+  echo "./createRelease.sh 2.0.0 2.0.0"
+  echo "or"
+  echo "./createRelease.sh 2.3.5 2.3.5000"
+  exit 1
+fi
 
 UPSTREAM_VERSION="$1"
-UPSTREAM_CHECKSUM_FILE="${SCRIPTDIR}/packageInfo/upstreamChecksums/${UPSTREAM_VERSION}.txt"
-VERSION_MAPPING_FILE="${SCRIPTDIR}/packageInfo/esy-ocaml-versionMapping/${UPSTREAM_VERSION}.upstream.txt"
+ESY_OCAML_VERSION="$2"
 
-echo "Creating esy release for upstream version: ($UPSTREAM_VERSION)"
+echo "Creating @esy-ocaml/bs-platform@${ESY_OCAML_VERSION} for upstream version: ($UPSTREAM_VERSION)"
 
-if [ -f "${UPSTREAM_CHECKSUM_FILE}" ]; then
-  true
-else
-  echo "No checksum located at: ${UPSTREAM_CHECKSUM_FILE}"
-  exit 1
-fi
-
-if [ -f "${VERSION_MAPPING_FILE}" ]; then
-  true
-else
-  echo "No version mapping file located at: ${VERSION_MAPPING_FILE}"
-  exit 1
-fi
-
-ESY_OCAML_VERSION=$(cat ${VERSION_MAPPING_FILE})
-SHA_SUM=$(cat "${UPSTREAM_CHECKSUM_FILE}")
 RELEASE_FILE="bs-platform-${UPSTREAM_VERSION}.tgz"
 BS_PLATFORM_URL="https://registry.npmjs.org/bs-platform/-/${RELEASE_FILE}"
 
-wget "$BS_PLATFORM_URL"
-SHA_SUM_CHECKED=$(shasum $RELEASE_FILE)
-
-if [ "$SHA_SUM_CHECKED" != "${SHA_SUM}  ${RELEASE_FILE}" ]; then
-  echo "ERROR!!!"
-  echo "The shasum didn't check out: Expecting: $SHA_SUM  $RELEASE_FILE but found: $SHA_SUM_CHECKED"
-  echo "ERROR!!!"
-  exit 1
-fi
+curl "$BS_PLATFORM_URL" -O
 
 # Remove any left over package
 rm -rf ./package/
@@ -102,6 +86,36 @@ if [ "${UPSTREAM_VERSION}" == "2.1.0" ]; then
   mv ./package/lib/bsb.ml ./package/lib/bsb.ml.orig
   patch ./package/lib/bsb.ml.orig -i "./packageInfo/upstreamPatches/${UPSTREAM_VERSION}/lib/bsb.ml.patch" -o ./package/lib/bsb.ml
 fi
+
+if [ "${UPSTREAM_VERSION}" == "2.2.1" ]; then
+  echo "Removing executable files in release:"
+ 
+  rm package/lib/bsb.darwin
+  rm package/lib/bsb_helper.darwin
+  rm package/lib/bsc.darwin
+  rm package/lib/bsppx.darwin
+  rm package/lib/reactjs_jsx_ppx_2.darwin
+  rm package/lib/refmt.darwin
+  rm package/lib/refmt3.darwin
+
+  # Prebuilts for linux aren't distributed.
+  # rm package/lib/bsb.linux64
+  # rm package/lib/bsb_helper.linux64
+  # rm package/lib/bsc.linux64
+  # rm package/lib/bsppx.linux64
+  # rm package/lib/reactjs_jsx_ppx_2.linux64
+  # rm package/lib/refmt.linux64
+  # rm package/lib/refmt3.linux64
+
+  rm package/lib/bsb.win
+  rm package/lib/bsb_helper.win
+  rm package/lib/bsc.win
+  rm package/lib/bsppx.win
+  rm package/lib/reactjs_jsx_ppx_2.win
+  rm package/lib/refmt.win
+  rm package/lib/refmt3.win
+fi
+
 
 # Remove the heavy docs and site directories
 rm -rf package/site/
